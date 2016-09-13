052831827eca9668d093aa92b6e0640614e4c7d6
Paula <villanuevapaulasofia@gmail.com>
2016-09-13 14:29:03 -0300

    mas declarativo

--- TP1/preprocessor.py
+++ TP1/preprocessor.py
@@ -14,9 +14,9 @@ class OutlierFilter:
     def process(self, features):
         features_std = np.std(features, axis=0)
         feature_means = np.mean(features, axis=0)
-        features_wo_outliers = [item for item in features if self.isOutlier(item,feature_means,features_std)]
+        features_wo_outliers = filter(self.isOutlier(feature_means,features_std), features)
         print(len(features), len(features_wo_outliers))
         return features_wo_outliers
 
-    def isOutlier(self, item, means, std):
-        return (2*std + means - np.abs(item) > 0).all()
+    def isOutlier(self, means, std):
+        return lambda item: (2*std + means - np.abs(item) > 0).all()
