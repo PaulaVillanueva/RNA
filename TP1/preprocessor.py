df3979d3c3173afe24b16c3ef10b1ca96d265a54
Paula <villanuevapaulasofia@gmail.com>
2016-09-13 17:04:19 -0300

    correccion filtro outliers, histograma de un feature

--- TP1/preprocessor.py
+++ TP1/preprocessor.py
@@ -14,9 +14,16 @@ class OutlierFilter:
     def process(self, features):
         features_std = np.std(features, axis=0)
         feature_means = np.mean(features, axis=0)
-        features_wo_outliers = filter(self.isOutlier(feature_means,features_std), features)
+        features_wo_outliers = np.array(filter(self.isOutlier(feature_means,features_std), features))
+
         print(len(features), len(features_wo_outliers))
+        #histograma de la 3era feature con cortes de outliers
+        plt.hist(features[:,2],bins=np.max(features[:,2])-np.min(features[:,2]))
+        plt.axvline(feature_means[2]-2*features_std[2], color='b', linestyle='dashed', linewidth=2)
+        plt.axvline(feature_means[2]+2*features_std[2], color='b', linestyle='dashed', linewidth=2)
+        plt.show()
+        
         return features_wo_outliers
 
     def isOutlier(self, means, std):
-        return lambda item: (2*std + means - np.abs(item) > 0).all()
+        return lambda item: (np.abs(means - item) < 2*std).all()
