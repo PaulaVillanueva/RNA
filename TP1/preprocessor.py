32a13329ed869517789cb82d44f997bb1d14b93f
Paula <villanuevapaulasofia@gmail.com>
2016-09-13 14:12:42 -0300

    filtro outliers, falta chequear

--- TP1/preprocessor.py
+++ TP1/preprocessor.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import matplotlib.pyplot as plt
 
 class FeatureNormalizer:
 
@@ -10,6 +10,13 @@ class FeatureNormalizer:
         return features_normalized
 
 class OutlierFilter:
-    def process(self, features):
-        return features
 
+    def process(self, features):
+        features_std = np.std(features, axis=0)
+        feature_means = np.mean(features, axis=0)
+        features_wo_outliers = [item for item in features if self.isOutlier(item,feature_means,features_std)]
+        print(len(features), len(features_wo_outliers))
+        return features_wo_outliers
+
+    def isOutlier(self, item, means, std):
+        return (2*std + means - np.abs(item) > 0).all()
--- TP1/tests.py
+++ TP1/tests.py
@@ -2,21 +2,23 @@ import numpy as np
 
 import preprocessor
 import sigmoid
+import ej1_data_loader
 from feed_forward_solver import FeedForwardSolver
 
-X = np.genfromtxt('/Users/bpanarello/Dropbox/RN/P2/letters_1.txt', delimiter=" ")
-B = np.ones((26, 1)) * -1
-X = np.concatenate((B, X), axis=1)
-Z = np.genfromtxt('/Users/bpanarello/Dropbox/RN/P2/letters_tags.txt', delimiter=" ")
-W = np.random.rand(26, 5)/100
+# X = np.genfromtxt('/Users/bpanarello/Dropbox/RN/P2/letters_1.txt', delimiter=" ")
+# B = np.ones((26, 1)) * -1
+# X = np.concatenate((B, X), axis=1)
+# Z = np.genfromtxt('/Users/bpanarello/Dropbox/RN/P2/letters_tags.txt', delimiter=" ")
+# W = np.random.rand(26, 5)/100
 
 
-solver = FeedForwardSolver([W], sigmoid.sigmoid_array)
-Y = solver.solve_sample_and_return_output(X)
-print Y
+# solver = FeedForwardSolver([W], sigmoid.sigmoid_array)
+# Y = solver.solve_sample_and_return_output(X)
+# print Y
 
-X = np.genfromtxt('/Users/bpanarello/Dropbox/RN/repo/RNA/TP1/ds/tp1_ej1_training.csv', delimiter=",")
-n = preprocessor.FeatureNormalizer()
-N = n.normalize_features(X)
+
+X = np.genfromtxt('./ds/tp1_ej1_training.csv', delimiter=",")
+loader = ej1_data_loader.Ej1DataLoader()
+loader.LoadData()
 print N
 
