28f0d95209fb4acd24ddf1e87181c5673d0a761d
Paula <villanuevapaulasofia@gmail.com>
2016-10-16 11:18:33 -0300

    practica4, hay que chequear y optimizar

---
+++ unsupervised learning/practica4.py
@@ -0,0 +1,60 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from us_learning import HebbianNN
+
+# TODO: optimizar y chequear
+
+def normalize(v):
+    means = np.mean(v, axis=0)
+    stds = np.std(v, axis=0)
+    return (v - means) / stds
+
+
+# train
+
+A = np.random.random_integers(1, 100, 6)
+print("A:", A)
+
+epochs = 200
+
+DS = []
+for t in range(epochs):
+    X = [np.random.uniform(-a, a) for a in A]
+    DS.append(X)
+
+DS = np.array(DS, dtype=float)
+DS = normalize(DS)
+
+HB = HebbianNN(6, 4)
+
+# OjaM
+
+we = HB.train(DS, epochs, True)
+
+print("weights OjaM: ")
+print(we)
+plt.matshow(we, cmap='hot', vmin=-1, vmax=1)
+plt.colorbar()
+plt.show()
+
+outputs = [np.dot(x.transpose(), we) for x in DS]
+
+print("mean: ", np.mean(outputs, axis=0))
+print("std: ", np.std(outputs, axis=0))
+print("var: ", np.var(outputs, axis=0))
+
+# Sanger
+
+we = HB.train(DS, epochs)
+
+print("weights Sanger: ")
+print(we)
+plt.matshow(we, cmap='hot', vmin=-1, vmax=1)
+plt.colorbar()
+plt.show()
+
+outputs = [np.dot(x.transpose(), we) for x in DS]
+
+print("mean: ", np.mean(outputs, axis=0))
+print("std: ", np.std(outputs, axis=0))
+print("var: ", np.var(outputs, axis=0))
---
+++ unsupervised learning/us_learning.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+class HebbianNN:
+    def __init__(self, inputs, outputs):
+
+        self._inputs = inputs
+        self._outputs = outputs
+        self._weights = np.random.uniform(-0.5, 0.5, (inputs, outputs))
+
+    def train(self, ds, epochs, oja=False):
+        weights = self._weights
+        for e in range(1, epochs + 1):
+
+            lr = 0.1 / e
+
+            for x in ds:
+                y = np.dot(x.transpose(), weights)
+                dw = np.zeros((self._inputs, self._outputs), dtype=float)
+
+                for j in range(self._outputs):
+                    for i in range(self._inputs):
+                        xi_mo = 0
+                        for k in range(oja and self._outputs or (j + 1)):
+                            xi_mo += y.transpose()[k] * weights[i][k]
+                        dw[i][j] = lr * y.transpose()[j] * (x[i] - xi_mo)
+
+                weights += dw
+
+        return weights
+
