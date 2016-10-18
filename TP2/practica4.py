ea9c697f6066f86f2ff3063dcfcfc235cf10dd01
Paula <villanuevapaulasofia@gmail.com>
2016-10-18 00:27:37 -0300

    correccion y parametros

--- TP2/practica4.py
+++ TP2/practica4.py
@@ -2,19 +2,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 from us_learning import HebbianNN
 
+# los datos tienen que estar centralizados, no normalizados
+# en este caso, se supone que ya estarian centrados por la uniforme alrededor del 0
 
-def normalize(v):
+def centralize(v):
     means = np.mean(v, axis=0)
-    stds = np.std(v, axis=0)
-    return (v - means) / stds
-
+    return v - means
 
 # train
 
 A = np.random.random_integers(1, 100, 6)
+# A[0] = A[1]
 print("A:", A)
 
-epochs = 200
+epochs = 1000
+eps = 0.0001
+lrcons = 0.0001
 
 DS = []
 for t in range(epochs):
@@ -22,22 +25,28 @@ for t in range(epochs):
     DS.append(X)
 
 DS = np.array(DS, dtype=float)
-DS = normalize(DS)
+DS = centralize(DS)
 
-HB = HebbianNN(6, 4)
+HB = HebbianNN(6, 4, 0.5)
 
 # OjaM
 
-we = HB.train(DS, 0.001, epochs, True)
+we = HB.train(DS, eps, lrcons, epochs, True)
 
-print("weights OjaM: ")
-print(we)
-print("check orthogonality: ", HB.orthogonal(we,0.001))
+print we
+print "check orthogonality: ", HB.orthogonal(we,eps)
+print "check norm == 1: ", np.linalg.norm(we, axis=0)
+
 plt.matshow(we, cmap='hot', vmin=-1, vmax=1)
 plt.colorbar()
 plt.show()
 
-outputs = [np.dot(x.transpose(), we) for x in DS]
+outputs = np.array([np.dot(x.transpose(), we) for x in DS])
+
+# print outputs
+# plt.matshow(outputs[:,0], outputs[:,1], cmap='hot', vmin=-4, vmax=4)
+# plt.colorbar()
+# plt.show()
 
 print("mean: ", np.mean(outputs, axis=0))
 print("std: ", np.std(outputs, axis=0))
@@ -45,17 +54,31 @@ print("var: ", np.var(outputs, axis=0))
 
 # Sanger
 
-we = HB.train(DS, 0.001, epochs)
+eps = 0.007
 
-print("weights Sanger: ")
-print(we)
-print("check orthogonality: ", HB.orthogonal(we,0.001))
+we = HB.train(DS, eps, lrcons, epochs)
+
+print we
+print "check orthogonality: ", HB.orthogonal(we,eps)
+print "check norm == 1: ", np.linalg.norm(we, axis=0)
+
+
 plt.matshow(we, cmap='hot', vmin=-1, vmax=1)
 plt.colorbar()
 plt.show()
 
-outputs = [np.dot(x.transpose(), we) for x in DS]
+outputs = np.array([np.dot(x.transpose(), we) for x in DS])
 
 print("mean: ", np.mean(outputs, axis=0))
 print("std: ", np.std(outputs, axis=0))
 print("var: ", np.var(outputs, axis=0))
+
+
+# plt.plot(outputs[:,0], outputs[:,1], 'ro')
+# plt.show()
+
+# plt.plot(outputs[:,1], outputs[:,2], 'ro')
+# plt.show()
+
+# plt.plot(outputs[:,2], outputs[:,3], 'ro')
+# plt.show()
--- TP2/us_learning.py
+++ TP2/us_learning.py
@@ -4,17 +4,17 @@ import numpy as np
 # TODO: optimizar y chequear
 
 class HebbianNN:
-    def __init__(self, inputs, outputs):
+    def __init__(self, inputs, outputs, wint):
 
         self._inputs = inputs
         self._outputs = outputs
-        self._weights = np.random.uniform(-0.5, 0.5, (inputs, outputs))
+        self._weights = np.random.uniform(-wint, wint, (inputs, outputs))
 
     def train_wepochs(self, ds, epochs, oja=False):
         weights = self._weights
         for e in range(1, epochs + 1):
 
-            lr = 0.1 / e
+            lr = 0.0001 / e
 
             for x in ds:
                 y = np.dot(x.transpose(), weights)
@@ -31,12 +31,13 @@ class HebbianNN:
 
         return weights
         
-    def train(self, ds, eps, max_epochs, oja=False):
-        weights = self._weights
+    def train(self, ds, eps, lrcons, max_epochs, oja=False):
+        weights = np.matrix.copy(self._weights)
+        print weights
         e = 0
         while (not self.orthogonal(weights,eps)) and e < max_epochs:
             e+=1
-            lr = 0.1 / e
+            lr = lrcons / e
 
             for x in ds:
                 y = np.dot(x.transpose(), weights)
